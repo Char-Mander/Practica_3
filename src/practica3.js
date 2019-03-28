de5ca6763b739b758018335140edd553819d4895
Char-Mander <36884991+Char-Mander@users.noreply.github.com>
2019-03-28 12:55:20 +0100

    quitada un a s

--- src/practica3.js
+++ src/practica3.js
@@ -1,6 +1,6 @@
 //Código de la práctica 3
 
-windows.addEventListener('load', function () {
+window.addEventListener('load', function () {
     // Set up an instance of the Quintus engine and include
     // the Sprites, Scenes, Input and 2D module. The 2D module
     // includes the`TileLayer`class as well as the`2d`componet.
