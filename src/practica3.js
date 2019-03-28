9351143f696cd7cf14dd73486e1f05cb3140fb81
Char-Mander <36884991+Char-Mander@users.noreply.github.com>
2019-03-28 15:32:46 +0100

    aVANCES EN LA PRÁCTICA

--- src/practica3.js
+++ src/practica3.js
@@ -1,18 +1,15 @@
 //Código de la práctica 3
 
-var game =  function () {
+var game = function () {
     // Set up an instance of the Quintus engine and include
     // the Sprites, Scenes, Input and 2D module. The 2D module
     // includes the`TileLayer`class as well as the`2d`componet.
-    console.log("entra");
+
     var Q = window.Q = Quintus({
-        audioSupported: ['mp3', 'ogg'],
+        audioSupported: ['mp3'],
         development: true,
-        imagesPath: "/images/",
-        audioPath: "/audio/",
-        dataPath: "/data/",
     })
-        .include("scenes, sprites, input, ui, touch")
+        .include('Scenes, Sprites, Input, UI, Touch, Audio, Anim, 2D')
         // Maximize this game to whatever the size of the browser is
         .setup({
             width: 320,
@@ -40,10 +37,9 @@ var game =  function () {
     // ## Asset Loading and Game Launch
     // Q.load can be called at any time to load additional assets
     // assets that are already loaded will be skipped
-    // The callback will be triggered when everything is loaded
-    Q.load("bg.png, bloopa.json, bloopa.png, coin.json, coin.mp3, coin.png, goomba.json, " + 
-    "goomba.png, level.tmx, mainTitle.png, mario_small.json, mario_small.png, music_main.mp3, " +
-    "music_die.mp3, music_level_complete.mp3, princess.png, tiles.png",
+    // The callback will be triggered when everything is loaded coin.mp3,  level.tmx, music_main.mp3, music_die.mp3, music_level_complete.mp3,
+    Q.load("bg.png, bloopa.json, bloopa.png, coin.png, coin.json, goomba.json, goomba.png, " +
+        "mainTitle.png, mario_small.json, mario_small.png, princess.png, tiles.png",
         function () {
             // Sprites sheets can be created manually
             // Or from a .json asset that defines sprite locations
@@ -51,44 +47,50 @@ var game =  function () {
             Q.compileSheets("coin.png", "coin.json");
             Q.compileSheets("bloopa.png", "bloopa.json");
             Q.compileSheets("goomba.png", "goomba.json");
+            
             Q.stageScene("mainTitle");
             // Finally, call stageScene to run the game
 
             Q.stageScene("level1");
         });
 
-       /* loadMario(Q);
-        loadPrincessPeach(Q);
-    
-        loadDefaultEnemy(Q);
-        loadGoomba(Q);
-        loadBloopa(Q);
-    
-        loadCoin(Q);
-    
-        loadEndGame(Q);
-        loadMainTitle(Q);
-        loadHUB(Q);
-    
-        loadLevel1(Q);*/
-    /* Q.load("level.tmx", function(){
+    /* loadMario(Q);
+     loadPrincessPeach(Q);
  
-     });  
- */
+     loadDefaultEnemy(Q);
+     loadGoomba(Q);
+     loadBloopa(Q);
+ 
+     loadCoin(Q);
+ 
+     loadEndGame(Q);
+     loadMainTitle(Q);
+     loadHUB(Q);
+ 
+     loadLevel1(Q);*/
+
 
 
 
     // ## Player Sprite
     // The very basic player sprite, this is just a normal sprite
     // using the player sprite sheet with default controls added to it.
-   /* Q.Sprite.extend("Player", {
+    Q.Sprite.extend("Mario", {
         // the init constructor is called on creation
         init: function (p) {
             // You can call the parent's constructor with this._super(..)
             this._super(p, {
-                sheet: "player", // Setting a sprite sheet sets sprite width and height
-                x: 410, // You can also set additional properties that can
-                y: 90 // be overridden on object creation
+                sprite: 'mario animation',
+                sheet: "mario",
+                x: 150, // You can also set additional properties that can
+                y: 380, // be overridden on object creation
+                direction: 'right',
+                jumpSpeed: -400,
+                speed: 200,
+                vy: 10,
+                died: false,
+                movement: true
+
             });
             // Add in pre-made components to get up and running quickly
             // The `2d` component adds in default 2d collision detection
@@ -97,29 +99,96 @@ var game =  function () {
             // default input actions (left, right to move, up or action to jump)
             // It also checks to make sure the player is on a horizontal surface before
             // letting them jump.
-            this.add('2d, platformerControls');
+            this.add('2d, platformerControls, animation, tween');
+            this.on("drag");
+
+
             // Write event handlers to respond hook into behaviors.
 
             // hit.sprite is called everytime the player collides with a sprite
             this.on("hit.sprite", function (collision) {
                 // Check the collision, if it's the Tower, you win!
-                if (collision.obj.isA("Tower")) {
-                    Q.stageScene("endGame", 1, { label: "You Won!" });
+                if (collision.obj.isA("Princess")) {
+                    this.p.movement = false;
+                    Q.audio.stop('music_main.mp3');
+                    Q.audio.play('music_level_complete.mp3');
+                    Q.stageScene('endGame', 1, { label: 'You win!' });
+                }
+                else {
+                    Q.audio.stop('music_main.mp3');
+                    if (!this.p.died) {
+                        Q.audio.play('music_die.mp3');
+                    }
+                    this.p.died = true;
+                    this.p.movement = false;
+                    this.p.speed = 0;
+                    this.p.jumpSpeed = 0;
+                    this.animate({ y: this.p.y - 100, angle: 0 }, 0.3);
+                    this.animate({ x: this.p.x, y: 0, angle: 0 }, 0.5);
                     this.destroy();
+                    Q.stageScene('endGame', 1, { label: 'Game Over' });
                 }
             });
+        },
+
+        drag: function (touch) {
+            this.p.x = touch.origX + touch.dx;
+            this.p.y = touch.origY + touch.dy;
+        },
+
+        step: function (dt) {
+            if (this.p.died) {
+                this.play('death');
+                this.p.speed = 0;
+                this.p.jumpSpeed = 0;
+            } else {
+                if (this.p.movement) {
+                    if (this.p.vy !== 0) {
+                        this.play('jump_' + this.p.direction);
+                    } else if (this.p.vx !== 0) {
+                        this.play('walk_' + this.p.direction);
+                    } else {
+                        this.play('stand_' + this.p.direction);
+                    }
+
+                    /*  if(this.p.y > escenario){
+                          this.trigger('death');
+                      }*/
+                }
+
+                else {
+                    this.play('stand_right');
+                    this.p.speed = 0;
+                    this.p.jumpSpeed = 0;
+                }
+            }
         }
     });
 
-    var player1 = new Q.Player();
+    //var mario = new Q.Mario();
 
-    // ## Tower Sprite
+    //Animación de Mario
+
+    Q.animations('mario animation', {
+        'walk_right': { frames: [1, 2, 3], rate: 1 / 7 },
+        'walk_left': { frames: [15, 16, 17], rate: 1 / 7 },
+        'stand_right': { frames: [0], loop: false },
+        'stand_left': { frames: [14], loop: false },
+        'jump_right': { frames: [4], loop: false },
+        'jump_left': { frames: [18], loop: false },
+        'death': { frames: [12], loop: false }
+    });
+
+
+    // ## Princess Sprite
     // Sprites can be simple, the Tower sprite just sets a custom sprite sheet
-    Q.Sprite.extend("Tower", {
+    Q.Sprite.extend("Princess", {
         init: function (p) {
-            this._super(p, { sheet: 'tower' });
+            this._super(p, { sheet: 'princess' });
         }
     });
+
+    /*
     // ## Enemy Sprite
     // Create the Enemy class to add in some baddies
     Q.Sprite.extend("Enemy", {
@@ -146,7 +215,7 @@ var game =  function () {
             });
         }
     });
-
+        */
 
     // ## Level1 scene
     // Create a new scene called level 1
@@ -159,16 +228,15 @@ var game =  function () {
             sheet: 'tiles'
         }));
         // Create the player and add them to the stage
-        var player = stage.insert(new Q.Player());
+        var mario = stage.insert(new Q.Mario());
         // Give the stage a moveable viewport and tell it
         // to follow the player.
-        //Editar para que siga a Mario
-        stage.add("viewport").follow(player);
+        stage.add("viewport").follow(mario);
         // Add in a couple of enemies
-        stage.insert(new Q.Enemy({ x: 700, y: 0 }));
-        stage.insert(new Q.Enemy({ x: 800, y: 0 }));
+      //  stage.insert(new Q.Enemy({ x: 700, y: 0 }));
+        //stage.insert(new Q.Enemy({ x: 800, y: 0 }));
         // Finally add in the tower goal
-        stage.insert(new Q.Tower({ x: 180, y: 50 }));
+        stage.insert(new Q.Princess({ x: 180, y: 50 }));
     });
     // To display a game over / game won popup box,
     // create a endGame scene that takes in a `label` option
@@ -191,13 +259,11 @@ var game =  function () {
             Q.clearStages();
             Q.stageScene('level1');
         });
+        6
         // Expand the container to visibily fit it's contents
         // (with a padding of 20 pixels)
         container.fit(20);
     });
 
-
-
-*/
 
 }
